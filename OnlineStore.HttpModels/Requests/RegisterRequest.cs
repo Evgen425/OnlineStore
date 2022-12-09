bc15768af4de7a35bee4660acd1848a58851eb7c
Izma_bd21@itstepru.onmicrosoft.com <Izma_bd21@itstepru.onmicrosoft.com>
2022-12-09 13:40:52 +0500

    Добавлен проект Domain; Сущности перенесены в проект Domain;

--- OnlineStore.HttpModels/Requests/RegisterRequest.cs
+++ OnlineStore.HttpModels/Requests/RegisterRequest.cs
@@ -4,20 +4,20 @@ namespace OnlineStore.Models.Requests;
 
 public class RegisterRequest
 {
-    [Required] public string Name { get; set; }
+    [Required(ErrorMessage = "Name is required")] 
+    public string Name { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Email is required")]
     [DataType(DataType.EmailAddress)]
     public string Email { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Password is required")]
     [MinLength(6)]
-    [DataType(DataType.Password)]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$")]
     public string Password { get; set; }
 
     [Required]
     [Compare("Password", ErrorMessage = "Пароли не соответствуют.")]
-    [MinLength(6)]
-    [DataType(DataType.Password)]
+   
     public string ConfirmPassword { get; set; }
 }
